f043f36ddbe4200ed4b24b0cc0cb952485be0608
dnjoe96 <donjoedbest@gmail.com>
2021-12-06 07:09:40 -0800

    Fix exit condition for unknown command

--- d_function.c
+++ d_function.c
@@ -60,5 +60,8 @@ int d_function(char **argv, stack_t **stack, unsigned int line)
 		}
 	}
 	fprintf(stderr, "L%d: unknown instruction %s\n", line, argv[0]);
+	free(stack);
+	free(argv);
+	exit(EXIT_FAILURE);
 	return (0);
 }
